404d29cc81e6ddb24a31f86b5c83e337bca2fe59
Devtahas <meysamsaiydavi2@gmail.com>
2026-08-30 00:10:30 +0330

    fix: restore flutter-plugin-loader in settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -18,7 +18,7 @@ pluginManagement {
 }
 
 plugins {
-    id("dev.flutter.flutter-gradle-plugin") version "1.0.0"
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "8.9.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
