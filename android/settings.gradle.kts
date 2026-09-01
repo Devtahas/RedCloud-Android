d71e5d527a2c012ee50c42844ab90fe361775758
Devtahas <meysamsaiydavi2@gmail.com>
2026-09-01 16:39:12 +0330

    fix: remove aliyun mirrors to fix 502 bad gateway on GitHub Actions

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -10,9 +10,6 @@ pluginManagement {
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 
     repositories {
-        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
-        maven { url = uri("https://maven.aliyun.com/repository/public") }
-        maven { url = uri("https://maven.aliyun.com/repository/google") }
         google()
         mavenCentral()
         gradlePluginPortal()
@@ -29,8 +26,6 @@ plugins {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
     repositories {
-        maven { url = uri("https://maven.aliyun.com/repository/public") }
-        maven { url = uri("https://maven.aliyun.com/repository/google") }
         google()
         mavenCentral()
         maven { url = uri("https://storage.googleapis.com/download.flutter.io") }
